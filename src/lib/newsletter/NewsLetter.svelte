e0a0f625425c6e288e166a1b114147dee547ef8e
Emil Sabri <emil.sabri@gmail.com>
2022-09-29 16:42:34 -0700

    Moved newsletter form to seperate component and integrated SendInBlue API

---
+++ src/lib/newsletter/NewsLetter.svelte
@@ -0,0 +1,26 @@
+
+<script>
+    const API_KEY = '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'
+    let email = ''
+
+    async function addToMailChimp() {
+       const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': API_KEY
+        },
+        body: JSON.stringify({
+            email: email
+        })
+       }
+        const response = await fetch('https://api.sendinblue.com/v3/contacts', options);
+    }
+</script>
+
+<p>Sign up to enjoy a live case study of success</p>
+    <form method="POST" on:submit|preventDefault={addToMailChimp}>
+        <input bind:value="{email}" name="email" type="email" placeholder="you@gmail.com">
+        <button>Subscribe</button>
+    </form>
+<p>Newsletter - Get status updates on the roadmap, blogs, and future projects</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
+<script>
+    import NewsLetter from "../lib/newsletter/NewsLetter.svelte";
+</script>
+
 <section>
     <h1>Welcome to my domain - EmilSabri.com</h1>
     <h2>I've been expecting you...</h2>
     <img src="/images/boss.png" alt="Emil the Boss" />
+    <NewsLetter />
     
-    <p>Sign up to enjoy a live case study of success</p>
-    <form method="POST">
-        <input name="email" type="email" placeholder="you@gmail.com">
-        <button>Subscribe</button>
-    </form>
-    <p>Newsletter - Get status updates on the roadmap, blogs, and future projects</p>
 </section>
