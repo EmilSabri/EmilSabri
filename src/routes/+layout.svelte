54a5bf0768479908555acf79ef5c0ff2796a73c2
Emil Sabri <emil.sabri@gmail.com>
2022-09-29 14:23:28 -0700

    Socials component + Twitter icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import GoogleTag from "$lib/meta/GoogleTag.svelte"
+    import Socials from "$lib/socials/Socials.svelte"
 </script>
 
 <!-- Import analytics and other meta data/tags stuff here  -->
@@ -10,6 +11,9 @@
     <a href="/about">About</a>
     <a href="/projects">Projects</a>
     <!-- <a href="/roadmap">Roadmap</a> -->
+    <Socials />
 </nav>
+
+
 
 <slot></slot>
