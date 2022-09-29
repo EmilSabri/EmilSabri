cc239b2f613931ef4dedfe5586c53fbd479b7c65
Emil Sabri <emil.sabri@gmail.com>
2022-09-29 13:26:46 -0700

    Added GoogleTag to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,10 @@
+<script>
+    import GoogleTag from "$lib/meta/GoogleTag.svelte"
+</script>
+
+<!-- Import analytics and other meta data/tags stuff here  -->
+<GoogleTag />
+
 <nav>
     <a href="/">Home</a>
     <a href="/about">About</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,12 @@
-<!-- Todo - Socials (Twitter, LinkedIn, Tiktok, Youtube, Instagram) -->
-<!-- Todo - Socials API grab to display my profile-->
-<!-- Todo - Email Newsletter capture -->
-<!-- Todo - Roadmap: socials, youtube channel, projects, goals-->
-<!-- Todo - Favorite books -->
-<!-- Todo - Total unique page views, monthly page views -->
-
 <section>
-    <!--  Todo - Blue Pill (left side) Right Pill (right side) hovering up and down with left/right hands as backdrop -->
     <h1>Welcome to my domain - EmilSabri.com</h1>
     <h2>I've been expecting you...</h2>
     <img src="/images/boss.png" alt="Emil the Boss" />
-
+    
+    <p>Sign up to enjoy a live case study of success</p>
     <form method="POST">
         <input name="email" type="email" placeholder="you@gmail.com">
         <button>Subscribe</button>
     </form>
     <p>Newsletter - Get status updates on the roadmap, blogs, and future projects</p>
-
-
 </section>
